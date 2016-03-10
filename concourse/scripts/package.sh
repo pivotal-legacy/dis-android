50a87006712f6dd4c9e4c9dbb718d037eb181a25
Callum Stott <cs@pivotal.io>
2016-03-10 13:42:22 +0000

    Update package script to copy output

--- concourse/scripts/package.sh
+++ concourse/scripts/package.sh
@@ -2,3 +2,9 @@
 
 set -e
 export TERM=dumb # needed for Gradle: https://issues.gradle.org/browse/GRADLE-2634
+
+pushd github
+./gradlew assemble
+popd
+
+cp github/build/outputs/apk/github.apk package/dis.apk
